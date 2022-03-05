ecdeccec7b2c3e8328693c5ece6daa8f245a44ee
Kody Deda <kodydeda4@gmail.com>
2022-03-05 17:02:23 -0500

    Update iconsur.sh

--- iconsur.sh
+++ iconsur.sh
@@ -1,56 +1,17 @@
-# Original Program - https://github.com/rikumi/iconsur
+# Iconsur https://github.com/rikumi/iconsur
 
 # iOS -> BigSur
-sudo iconsur set /Applications/Microsoft\ Excel.app/
-sudo iconsur set /Applications/Microsoft\ Outlook.app/
-sudo iconsur set /Applications/Microsoft\ PowerPoint.app/
-sudo iconsur set /Applications/Microsoft\ Remote\ Desktop.app/
-sudo iconsur set /Applications/Microsoft\ Word.app/
-sudo iconsur set /Applications/Microsoft\ OneNote.app 
-sudo iconsur set /Applications/Microsoft\ Teams.app 
-sudo iconsur set /Applications/Unsplash\ Wallpapers.app 
-sudo iconsur set /Applications/Slack.app 
 sudo iconsur set /Applications/zoom.us.app 
-sudo iconsur set /Applications/Firefox.app 
-sudo iconsur set '/Applications/Google Chrome.app'
 
 # Adaptive
 sudo iconsur set /Applications/Icon\ Set\ Creator.app -l -s 0.8
-sudo iconsur set /Applications/Alt-C.app -l -s 0.8
-sudo iconsur set /Applications/SoundflowerBed.app -l -s 0.8
-sudo iconsur set /Applications/Backup\ and\ Sync.app -l -s 0.8
-sudo iconsur set /Applications/Scroll\ Reverser.app -l -s 0.8
-sudo iconsur set /Applications/Zeplin.app -l -s 0.8
-sudo iconsur set /Applications/Viscosity.app -l -s 0.8
-sudo iconsur set /Applications/Notion.app -l -s 0.8
-sudo iconsur set /Applications/Amethyst.app -l -s 0.8
 
 # Custom
-sudo iconsur set /Applications/Android\ Studio.app  -l -i /Users/kodydeda/Pictures/'appicons'/'AndroidStudio'.png
-sudo iconsur set '/Applications/Docker.app' -l -i /Users/kodydeda/Pictures/'appicons'/docker.png
-sudo iconsur set /Applications/Docker.app/Contents/MacOS/Docker\ Desktop.app -l -i /Users/kodydeda/Pictures/'appicons'/docker.png
-sudo iconsur set /Applications/Adobe\ Photoshop\ 2021/Adobe\ Photoshop\ 2021.app -l -i /Users/kodydeda/Pictures/'appicons'/Photoshop.png
-sudo iconsur set /Applications/Adobe\ Illustrator\ 2021/Adobe\ Illustrator.app -l -i /Users/kodydeda/Pictures/'appicons'/Illustrator.png
-sudo iconsur set /Applications/Guitar\ Pro\ 7.app -l -i /Users/kodydeda/Pictures/'appicons'/GuitarPro.png
-sudo iconsur set /Applications/Fluid.app -l -i /Users/kodydeda/Pictures/'appicons'/Fluid.png
-sudo iconsur set /Applications/Adblock\ Plus.app -l -i /Users/kodydeda/Pictures/'appicons'/Adblock.png
-sudo iconsur set /Applications/checkra1n.app -l -i /Users/kodydeda/Pictures/'appicons'/Checkra1n.png
-sudo iconsur set /Applications/OpenEmu.app -l -i /Users/kodydeda/Pictures/'appicons'/Openemu.png
-sudo iconsur set /Applications/Unite.app -l -i /Users/kodydeda/Pictures/'appicons'/Unite.png
-sudo iconsur set /Applications/VirtualBox.app -l -i /Users/kodydeda/Pictures/'appicons'/VirtualBox.png
-sudo iconsur set /Applications/Mapping\ Tonal\ Harmony\ Pro.app -l -i /Users/kodydeda/Pictures/'appicons'/mthp.png
-sudo iconsur set /Applications/balenaEtcher.app  -l -i /Users/kodydeda/Pictures/'appicons'/Balena.png
-sudo iconsur set /Applications/Dark\ Reader\ for\ Safari.app -l -i /Users/kodydeda/Pictures/'appicons'/darkreader.png
-sudo iconsur set /Applications/Magnet.app -l -i /Users/kodydeda/Pictures/'appicons'/magnet.png
-sudo iconsur set '/Applications/GitHub Desktop.app' -l -i /Users/kodydeda/Pictures/'appicons'/Github.png
-sudo iconsur set '/Applications/PyCharm CE.app' -l -i /Users/kodydeda/Pictures/'appicons'/Pycharm.png
-sudo iconsur set /Applications/IntelliJ\ IDEA\ CE.app -l -i /Users/kodydeda/Pictures/'appicons'/Idea.png
-sudo iconsur set /Applications/Dozer.app -l -i /Users/kodydeda/Pictures/'appicons'/Dozer.png
-sudo iconsur set /Applications/SwiftUI\ Companion.app -l -i /Users/kodydeda/Pictures/'appicons'/SwiftUICompanion.png
-sudo iconsur set /Applications/'Slippi Dolphin.app' -l -i /Users/kodydeda/Pictures/'appicons'/Slippi.png
-sudo iconsur set /Applications/'GIPHY CAPTURE.app'  -l -i /Users/kodydeda/Pictures/'appicons'/'Giphy'.png
-sudo iconsur set /Applications/Logi\ Options.app -l -i /Users/kodydeda/Pictures/'appicons'/LogitechOptions.png
-sudo iconsur set /Applications/'Google Drive.app' -l -i /Users/kodydeda/Pictures/'appicons'/'GoogleDrive'.png
+sudo iconsur set '/Applications/Logi Options.app' -l -i '/Users/kodydeda/Library/Mobile Documents/com~apple~CloudDocs/Photos/AppIcons/LogitechOptions.png'
+sudo iconsur set '/Applications/Magnet.app' -l -i '/Users/kodydeda/Library/Mobile Documents/com~apple~CloudDocs/Photos/AppIcons/magnet.png'
+sudo iconsur set '/Applications/GitHub Desktop.app' -l -i '/Users/kodydeda/Library/Mobile Documents/com~apple~CloudDocs/Photos/AppIcons/Github.png'
 
 # Update Icons
 sudo iconsur cache
+
+
