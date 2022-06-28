ea640cc06c2f13d25759f446bb70cab5b7e31a33
Kody Deda <kodydeda4@gmail.com>
2022-06-28 10:45:16 -0400

    Update iconsur.sh

--- iconsur.sh
+++ iconsur.sh
@@ -2,16 +2,15 @@
 
 # iOS -> BigSur
 sudo iconsur set /Applications/zoom.us.app 
+sudo iconsur set /Applications/notion.app 
 
 # Adaptive
-sudo iconsur set /Applications/Icon\ Set\ Creator.app -l -s 0.8
+sudo iconsur set /Applications/Icon\ Set\ Creator.app 
+sudo iconsur set /Applications/Vimari.app 
 
 # Custom
 sudo iconsur set '/Applications/Logi Options.app' -l -i '/Users/kodydeda/Library/Mobile Documents/com~apple~CloudDocs/Photos/AppIcons/LogitechOptions.png'
-sudo iconsur set '/Applications/Magnet.app' -l -i '/Users/kodydeda/Library/Mobile Documents/com~apple~CloudDocs/Photos/AppIcons/magnet.png'
 sudo iconsur set '/Applications/GitHub Desktop.app' -l -i '/Users/kodydeda/Library/Mobile Documents/com~apple~CloudDocs/Photos/AppIcons/Github.png'
 
 # Update Icons
 sudo iconsur cache
-
-
